5d93af0446b894e17ee236514ea188ec3cd6897b
aqim <iamaqim@gmail.com>
2020-01-17 10:10:50 +0100

    updated-master

--- reader.py
+++ reader.py
@@ -1,4 +1,4 @@
-# import pandas as p
+import abc
 
 #file object as a context manager
 class OpenFile():
@@ -15,9 +15,18 @@ class OpenFile():
     def __exit__(self, exc_type, exc_value, exc_traceback): 
         self.file.close()
     
+# class for my interface contract
+class ContractInterface(abc.ABC):
+    @abc.abstractmethod
+    def read_last_two_lines(self):
+        pass
+
+    @abc.abstractmethod
+    def readall(self):
+        pass
 
 # class to read opened file
-class ReadFile:
+class ReadFile(ContractInterface):
     def __init__(self, file_descriptor):
         self.fd= file_descriptor
         self.read_file = []
@@ -38,11 +47,9 @@ class ReadFile:
 
         
     def read_last_two_lines(self):
-        with open(self.filename) as f:
-            data = f.readlines()
-            tail = data[-2:]
-            two_tails = ''.join(tail) 
-            return two_tails
+        # for line in self.fd:
+        #     return self.fd
+        # # return self.read_file
 
 
 
@@ -64,7 +71,7 @@ class iterate_through_file:
 
 with OpenFile('iris.csv') as file_obj:
     abc=ReadFile(file_obj)
-    print(abc.read_first_two_lines())
+    print(abc.read_last_two_lines())
         
 
 
